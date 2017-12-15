772dd647d37de78723ade97f652a22da21d1b1e7
yuliaveres <juliaveresklia@gmail.com>
2017-12-15 11:37:51 +0100

    Prepare MessageRequestsController

--- Toshi/Controllers/MessageRequests/MessageRequestsViewController.swift
+++ Toshi/Controllers/MessageRequests/MessageRequestsViewController.swift
@@ -13,8 +13,158 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import Foundation
+import UIKit
+import SweetFoundation
+import SweetUIKit
 
-final class MessageRequestsViewController: UIViewController {
+final class MessagesRequestsViewController: SweetTableController {
 
+    private lazy var dataSource: ThreadsDataSource = {
+        let dataSource = ThreadsDataSource(target: .unacceptedThreadRequests)
+        dataSource.output = self
+
+        return dataSource
+    }()
+
+    private var idAPIClient: IDAPIClient {
+        return IDAPIClient.shared
+    }
+
+    override init(style: UITableViewStyle) {
+        super.init(style: style)
+
+        title = dataSource.title
+
+        loadViewIfNeeded()
+    }
+
+    required init?(coder _: NSCoder) {
+        fatalError()
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.delegate = self
+        tableView.dataSource = self
+
+        BasicTableViewCell.register(in: tableView)
+        tableView.tableFooterView = UIView(frame: .zero)
+        tableView.showsVerticalScrollIndicator = true
+        tableView.alwaysBounceVertical = true
+
+        dataSource.output = self
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        preferLargeTitleIfPossible(true)
+        tabBarController?.tabBar.isHidden = false
+    }
+
+    private func recipient(for thread: TSThread) -> TokenUser? {
+        guard let recipientAddress = thread.contactIdentifier() else { return nil }
+
+        var recipient: TokenUser?
+
+        if let userData = (Yap.sharedInstance.retrieveObject(for: recipientAddress, in: ThreadsDataSource.nonContactsCollectionKey) as? Data),
+            let deserialised = (try? JSONSerialization.jsonObject(with: userData, options: [])),
+            let json = deserialised as? [String: Any] {
+
+            recipient = TokenUser(json: json, shouldSave: false)
+        }
+
+        return recipient
+    }
+}
+
+extension MessagesRequestsViewController: UITableViewDataSource {
+
+    func tableView(_: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataSource.unacceptedThreadsCount
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        let cellConfigurator = CellConfigurator()
+
+        guard let thread = dataSource.unacceptedThread(at: indexPath) else { return UITableViewCell(frame: .zero) }
+
+        var avatar: UIImage?
+        var subtitle = "..."
+        var title = ""
+
+        if thread.isGroupThread() {
+            avatar = (thread as? TSGroupThread)?.groupModel.groupImage ?? UIImage(named: "avatar-placeholder")
+            title = thread.name()
+        } else if let recipient = recipient(for: thread) {
+            avatar = AvatarManager.shared.cachedAvatar(for: recipient.avatarPath) ?? UIImage(named: "avatar-placeholder")
+            title = recipient.nameOrDisplayUsername
+        }
+
+        if let message = thread.messages.last, let messageBody = message.body {
+            switch SofaType(sofa: messageBody) {
+            case .message:
+                subtitle = SofaMessage(content: messageBody).body
+            case .paymentRequest:
+                subtitle = SofaPaymentRequest(content: messageBody).body
+            default:
+                break
+            }
+        }
+
+        let cellData = TableCellData(title: title, subtitle: subtitle, leftImage: avatar, doubleActionImages: (firstImage: UIImage(named: "accept_thread_icon")!, secondImage: UIImage(named: "decline_thread_icon")!))
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellConfigurator.cellIdentifier(for: cellData.components), for: indexPath) as? BasicTableViewCell else { return UITableViewCell(frame: .zero) }
+        cellConfigurator.configureCell(cell, with: cellData)
+        cell.actionDelegate = self
+
+        return cell
+    }
+}
+
+extension MessagesRequestsViewController: BasicCellActionDelegate {
+
+    func didTapFirstActionButton(_ cell: BasicTableViewCell) {
+        // accept thread at given indexpath
+    }
+
+    func didTapSecondActionButton(_ cell: BasicTableViewCell) {
+        // decline thread at given indexpath
+    }
+}
+
+extension MessagesRequestsViewController: ThreadsDataSourceOutput {
+
+    func threadsDataSourceDidLoad() {
+        tableView.reloadData()
+    }
+}
+
+extension MessagesRequestsViewController: UITableViewDelegate {
+
+    func tableView(_: UITableView, estimatedHeightForRowAt _: IndexPath) -> CGFloat {
+        return UITableViewAutomaticDimension
+    }
+
+    func tableView(_: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        guard let thread = dataSource.unacceptedThread(at: indexPath) else { return }
+        let chatViewController = ChatViewController(thread: thread)
+        navigationController?.pushViewController(chatViewController, animated: true)
+    }
+
+    func tableView(_: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+        let action = UITableViewRowAction(style: .destructive, title: "Delete") { _, indexPath in
+            if let thread = self.dataSource.acceptedThread(at: indexPath) {
+
+                TSStorageManager.shared().dbReadWriteConnection?.asyncReadWrite { transaction in
+                    thread.remove(with: transaction)
+                }
+            }
+        }
+
+        return [action]
+    }
 }
